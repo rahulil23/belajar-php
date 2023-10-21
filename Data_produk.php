b246fd78927dd5017ca7fa8e5695bd092b1b4d30
rahulil23 <rahul.ratu662@gmail.com>
2023-10-22 00:19:13 +0700

    [FIX] serach and pagination

--- Data_produk.php
+++ Data_produk.php
@@ -1,23 +1,44 @@
 <?php
 include('koneksi.php');
 session_start();
-if ( !isset($_SESSION["login"])) {
+if (!isset($_SESSION["login"])) {
   header("location: login.php");
 
   exit;
 }
-//queri sql
-$query = "SELECT * FROM products";
+
+$limit = 4;
+
+// queri data dan pencarian
+$query = "SELECT * FROM products LIMIT $limit";
+if (isset($_GET['search']) && !empty($_GET['search'])) {
+  $search = $conn->real_escape_string($_GET['search']);
+  $query .= " WHERE 
+          LOWER(product_name) LIKE LOWER('%{$_GET['search']}%') OR
+          LOWER(category_id) LIKE LOWER('%{$_GET['search']}%') OR
+          LOWER(description) LIKE LOWER('%{$_GET['search']}%')";
+}
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1; // default halaman 1
+$offset = ($page - 1) * $limit; // menentukan lompatan data
+
+$query .= " OFFSET $offset";
+
+$result_count = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM products")); //menghitung jumlah data
+
+$total_pages = ceil($result_count / $limit); // membagi data dengan limit
+
+// Eksekusi query menampilkan data
 $result = $conn->query($query);
 
 if (!$result) {
-    die("Error dalam eksekusi query: " . $conn->error);
+  die("Error dalam eksekusi query: " . $conn->error);
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,127 +51,163 @@ if (!$result) {
   <!-- Theme style -->
   <link rel="stylesheet" href="asset/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!--Navbar-->
-  <?php
-  include('navbar.php');
-  ?>
-  <!--Navbar-->
+    <!--Navbar-->
+    <?php
+    include('navbar.php');
+    ?>
+    <!--Navbar-->
 
-  <!-- Main Sidebar Container -->
-  <?php
-  include('sidebar.php');
-  ?>
-  <!--/sidebar-->
+    <!-- Main Sidebar Container -->
+    <?php
+    include('sidebar.php');
+    ?>
+    <!--/sidebar-->
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0"> DATA PRODUK</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Produk</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-  
-    <!-- Main content -->
-    <div class="card">
-      <div class="card-header">
-        <a href="tambah_produk.php" class="btn btn-primary" role="button" title="Tambah Data"><i class="glyphicon glyphicon-plus"></i> Tambah</a>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0"> DATA PRODUK</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Produk</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
       </div>
-      <!-- /.card-header -->
-      <div class="card-body">
-        <table id="example1" class="table table-bordered table-striped">
-          <thead>
-          <tr>
-            <th>No</th>
-            <th>Nama produk</th>
-            <th>Kategori Produk</th>
-            <th>Kode produk</th>
-            <th>deskripsi</th>
-            <th>Harga</th>
-            <th>aksi</th>
-          </tr>
-          </thead>
-          <tbody>
-          <?php
-          $no = 1;
-          while ($row = $result->fetch_assoc()) 
-          {
-            
-            echo "<tr>";
-            echo "<td>" . $no . "</td>";
-            echo "<td>" . $row['product_name'] . "</td>";
-            echo "<td>" . $row['category_id'] . "</td>";
-            echo "<td>" . $row['product_code'] . "</td>";
-            echo "<td>" . $row['description'] . "</td>";
-            echo "<td>" . $row['price'] . "</td>";
-            echo "<td>";
-            echo "<a href='edit_produk.php?id=" . $row['id'] . "' class='btn btn-success'>Ubah</a>";
-            echo "<a href='hapus_produk.php?id=" . $row['id'] . "' class='btn btn-danger'>Hapus</a>";
-            echo "</td>";
-            echo "</tr>";
-            $no++;
-          }
-          
-        
-          ?>
-          </tbody>
-          <tfoot>
-          <tr>
-          <th>No</th>
-            <th>Nama produk</th>
-            <th>Kategori Produk</th>
-            <th>Kode produk</th>
-            <th>deskripsi</th>
-            <th>Harga</th>
-            <th>aksi</th>
-          </tr>
-          </tfoot>
-        </table>
-      </div>    
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <a href="tambah_produk.php" class="btn btn-primary" role="button" title="Tambah Data"><i class="glyphicon glyphicon-plus"></i> Tambah</a>
+                  <div class="float-right">
+                    <input type="text" id="search" class="form-control" placeholder="Cari Produk">
+                  </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama produk</th>
+                        <th>Kategori Produk</th>
+                        <th>Kode produk</th>
+                        <th>deskripsi</th>
+                        <th>Harga</th>
+                        <th>aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php
+                      $no = ($page - 1) * $limit + 1;
+                      while ($row = $result->fetch_assoc()) {
+
+                        echo "<tr>";
+                        echo "<td>" . $no . "</td>";
+                        echo "<td>" . $row['product_name'] . "</td>";
+                        echo "<td>" . $row['category_id'] . "</td>";
+                        echo "<td>" . $row['product_code'] . "</td>";
+                        echo "<td>" . $row['description'] . "</td>";
+                        echo "<td>" . $row['price'] . "</td>";
+                        echo "<td>";
+                        echo "<a href='edit_produk.php?id=" . $row['id'] . "' class='btn btn-success'>Ubah</a>";
+                        echo "<a href='hapus_produk.php?id=" . $row['id'] . "' class='btn btn-danger'>Hapus</a>";
+                        echo "</td>";
+                        echo "</tr>";
+                        $no++;
+                      }
+
+
+                      ?>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama produk</th>
+                        <th>Kategori Produk</th>
+                        <th>Kode produk</th>
+                        <th>deskripsi</th>
+                        <th>Harga</th>
+                        <th>aksi</th>
+                      </tr>
+                    </tfoot>
+                  </table>
+                  <ul class='pagination'>
+                    <?php
+                    for ($i = 1; $i <= $total_pages; $i++) {
+                      echo "<li class='page-item'><a class='page-link' href='?page=$i'>" . $i . "</a></li>";
+                    }
+                    ?>
+                  </ul>
+                </div>
+                <!--card body-->
+              </div>
+            </div>
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
     </div>
-    <!-- /.content -->
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    <?php
+    include('footer.php');
+    ?>
+    <!--/Main Footer-->
   </div>
-  <!-- /.content-wrapper -->
+  <!-- ./wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
+  <!-- REQUIRED SCRIPTS -->
 
-  <!-- Main Footer -->
-  <?php
-  include('footer.php');
-  ?>
-  <!--/Main Footer-->
-</div>
-<!-- ./wrapper -->
+  <!-- jQuery -->
+  <script src="asset/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="asset/dist/js/adminlte.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $("#search").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("table tbody tr").filter(function() {
+          var rowText = $(this).text().toLowerCase();
+          $(this).toggle(
+            rowText.indexOf(value) > -1
+          );
+        });
+      });
+    });
+  </script>
 
-<!-- REQUIRED SCRIPTS -->
 
-<!-- jQuery -->
-<script src="asset/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="asset/dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
--- tambah_produk.php
+++ tambah_produk.php
@@ -1,18 +1,17 @@
 <?php
 include('koneksi.php');
 session_start();
-if ( !isset($_SESSION["login"])) {
+if (!isset($_SESSION["login"])) {
   header("location: login.php");
 
   exit;
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Ambil data dari formulir
   $product_name = $_POST['product_name'];
   $category_id = $_POST['category_id'];
-  $product_code=$_POST['product_code'];
+  $product_code = $_POST['product_code'];
   $description = $_POST['description'];
   $price = $_POST['price'];
 
@@ -25,15 +24,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     header("location: Data_produk.php");
     exit;
   } else {
-      echo "Error: " . mysqli_error($conn);
+    echo "Error: " . mysqli_error($conn);
   }
-
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -46,114 +45,129 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   <!-- Theme style -->
   <link rel="stylesheet" href="asset/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!--Navbar-->
-  <?php
-  include('navbar.php');
-  ?>
-  <!--/Navbar-->
+    <!--Navbar-->
+    <?php
+    include('navbar.php');
+    ?>
+    <!--/Navbar-->
 
-  <!-- Main Sidebar Container -->
-  <?php
-  include('sidebar.php');
-  ?>
-  <!--/sidebar-->
-  
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0"> TAMBAH PRODUK</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Produk</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-  
-    <!-- Main content -->
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <form method="post" action="" enctype="multipart/form-data">
-            <div class="form-group">
-              <label for="product_name">Nama Produk:</label>
-              <input type="text" name="product_name" id="product_name" class="form-control" required>
-            </div>
-            <div class="form-group">
-              <?php
-                $query = "SELECT * FROM product_categories";
-                $result = mysqli_query($conn, $query);
-                $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    <!-- Main Sidebar Container -->
+    <?php
+    include('sidebar.php');
+    ?>
+    <!--/sidebar-->
 
-              ?>
-              <label for="category_id">Kategori Produk:</label>
-              <select name="category_id" id="category_id" class="form-control" required>
-              <option disabled selected value></option>
-                <?php
-                foreach ($categories as $category) {
-                  echo "<option value='" . $category['id'] . "'>" . $category['id'] . "</option>";
-                }
-                ?>
-              </select>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1 class="m-0"> TAMBAH PRODUK</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Produk</li>
+              </ol>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Tambahkan Produk</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form method="POST" action="" enctype="multipart/form-data">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="product_name">Nama Produk</label>
+                    <input type="text" name="product_name" id="product_name" class="form-control" placeholder="Nama Produk" required>
+                  </div>
+                  <div class="form-group">
+                    <?php
+                    $query = "SELECT * FROM product_categories";
+                    $result = mysqli_query($conn, $query);
+                    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+                    ?>
+                    <label for="category_id">Kategori Produk:</label>
+                    <select name="category_id" id="category_id" class="form-control" required>
+                      <option disabled selected value>Pilih Kategori</option>
+                      <?php
+                      foreach ($categories as $category) {
+                        echo "<option value='" . $category['id'] . "'>" . $category['category_name'] . "</option>";
+                      }
+                      ?>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="product_code">kode Produk:</label>
+                    <input type="text" name="product_code" id="product_code" class="form-control" placeholder="Kode produk" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="description">Deskripsi:</label>
+                    <textarea name="description" id="description" class="form-control" placeholder="Deskripsi Produk" required></textarea>
+                  </div>
+                  <div class="form-group">
+                    <label for="price">Harga:</label>
+                    <input type="text" name="price" id="price" class="form-control" placeholder="Harga" required>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+              </form>
             </div>
-            <div class="form-group">
-              <label for="product_code">kode Produk:</label>
-              <input type="text" name="product_code" id="product_code" class="form-control" required>
-            </div>
-            <div class="form-group">
-              <label for="description">Deskripsi:</label>
-              <textarea name="description" id="description" class="form-control" required></textarea>
-            </div>
-            <div class="form-group">
-              <label for="price">Harga:</label>
-              <input type="text" name="price" id="price" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Simpan</button>
-          </form>
+          </div>
         </div>
       </div>
+
+      <!-- /.content -->
     </div>
-    
-    <!-- /.content -->
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    <?php
+    include('footer.php');
+    ?>
+    <!--/Main Footer-->
   </div>
-  <!-- /.content-wrapper -->
+  <!-- ./wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
+  <!-- REQUIRED SCRIPTS -->
 
-  <!-- Main Footer -->
-  <?php
-  include('footer.php');
-  ?>
-  <!--/Main Footer-->
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="asset/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="asset/dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="asset/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="asset/dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
